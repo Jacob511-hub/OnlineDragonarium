216babd2de6e4324eb40c8f1386adb4af2978d70
Jacob511-hub <jacob511@outlook.com>
2025-03-12 16:21:18 -0230

    Added all 10 primary elements for filtering

--- backend/index.ts
+++ backend/index.ts
@@ -8,23 +8,20 @@ dotenv.config();
 const app = express();
 const port = 5000;
 
-// Set up the PostgreSQL connection pool
 const pool = new Pool({
   user: process.env.PG_USER,
   host: process.env.PG_HOST,
   database: process.env.PG_DATABASE,
   password: process.env.PG_PASSWORD,
-  port: Number(process.env.PG_PORT), // Convert to number
+  port: Number(process.env.PG_PORT),
 });
 
-// Middleware
 app.use(cors());
 
-// Sample API route to fetch data from a table
 app.get('/dragons', async (req, res) => {
   try {
-    const result = await pool.query('SELECT * FROM dragons'); // Replace with your table name
-    res.json(result.rows); // Send the data as JSON
+    const result = await pool.query('SELECT * FROM dragons');
+    res.json(result.rows);
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: 'Failed to fetch data' });
@@ -57,7 +54,6 @@ app.get('/dragons-by-element', async (req, res) => {
   }
 });
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
--- frontend/src/FilterContainer.tsx
+++ frontend/src/FilterContainer.tsx
@@ -2,6 +2,14 @@ import React from "react";
 import FilterButton from "./FilterButton";
 import IconFire from './images/Icon_Fire.webp';
 import IconPlant from './images/Icon_Plant.webp';
+import IconEarth from './images/Icon_Earth.webp';
+import IconCold from './images/Icon_Cold.webp';
+import IconAir from './images/Icon_Air.webp';
+import IconLightning from './images/Icon_Lightning.webp';
+import IconWater from './images/Icon_Water.webp';
+import IconMetal from './images/Icon_Metal.webp';
+import IconLight from './images/Icon_Light.webp';
+import IconDark from './images/Icon_Dark.webp';
 
 const FilterContainer: React.FC<{ onFilter: (name: string) => void }> = ({ onFilter }) => {
   return (
@@ -9,6 +17,14 @@ const FilterContainer: React.FC<{ onFilter: (name: string) => void }> = ({ onFil
       <div className="filter-box">
         <FilterButton src={IconFire} name="Fire" onClick={onFilter} />
         <FilterButton src={IconPlant} name="Plant" onClick={onFilter} />
+        <FilterButton src={IconEarth} name="Earth" onClick={onFilter} />
+        <FilterButton src={IconCold} name="Cold" onClick={onFilter} />
+        <FilterButton src={IconAir} name="Air" onClick={onFilter} />
+        <FilterButton src={IconLightning} name="Lightning" onClick={onFilter} />
+        <FilterButton src={IconWater} name="Water" onClick={onFilter} />
+        <FilterButton src={IconMetal} name="Metal" onClick={onFilter} />
+        <FilterButton src={IconLight} name="Light" onClick={onFilter} />
+        <FilterButton src={IconDark} name="Dark" onClick={onFilter} />
       </div>
     </div>
   );
