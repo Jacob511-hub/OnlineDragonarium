8253155c1f94ba84fbdbbef595c97a3a4c157940
Jacob511-hub <jacob511@outlook.com>
2025-05-23 13:28:28 -0230

    Temporarily set secure cookie to false for testing

--- backend/src/session-config.ts
+++ backend/src/session-config.ts
@@ -14,7 +14,7 @@ const sessionConfig = session({
   resave: false,
   saveUninitialized: true,
   cookie: {
-    secure: true,
+    secure: false,
     httpOnly: true, // Prevents client-side JS from accessing cookies
     maxAge: 1000 * 60 * 60 * 24, // 1 day
     sameSite: 'none'
