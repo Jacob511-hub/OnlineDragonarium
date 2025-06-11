cc734697015d84bb595af3c2de29c8bf6bfd703d
Jacob511-hub <jacob511@outlook.com>
2025-06-11 13:03:33 -0230

    Removed undefined alert message when registering

--- frontend/src/hooks/useRegister.ts
+++ frontend/src/hooks/useRegister.ts
@@ -9,7 +9,6 @@ const useRegister = (username: string, email: string, password: string) => {
     const handleRegister = async () => {
         try {
             const res = await axios.post(`${API_BASE_URL}/register`, { username, email, password });
-            alert(res.data.message);
             navigate("/login");
         } catch (err) {
             alert("Registration failed");
